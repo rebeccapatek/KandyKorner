660a91b58baaf9e060124cb9e44773ed7b83b492
rebeccapatek <rebeccapatek@gmail.com>
2020-01-23 15:23:53 -0600

    first commit

---
+++ src/components/products/Product.js
@@ -0,0 +1,10 @@
+import React from "react"
+import "./Product.css"
+
+export default ({ product }) => (
+    <section className="product">
+        <h3 className="product__name">{product.name}</h3>
+        <div className="product__type"> {product.productTypeId}</div>
+        
+    </section>
+)
